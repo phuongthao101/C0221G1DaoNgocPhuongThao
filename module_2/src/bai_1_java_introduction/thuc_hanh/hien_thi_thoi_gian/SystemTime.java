caf870cd49dbc3d5c4d16575035dd6cf6679325e
phuongthao101 <daongocphuongthao101@gmal.com>
2021-03-31 11:13:42 +0700

    upbai1

---
+++ module_2/src/bai_1_java_introduction/thuc_hanh/hien_thi_thoi_gian/SystemTime.java
@@ -0,0 +1,11 @@
+package bai_1_java_introduction.thuc_hanh.hien_thi_thoi_gian;
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+
+        System.out.println("Now is: " + now);
+    }
+}
+// Có thể sử dụng cách gõ tắt: psvm + TAB để tạo nhanh hàm main().
