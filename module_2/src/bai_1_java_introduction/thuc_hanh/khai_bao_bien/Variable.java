17513f850ac66b4a6218dd2042654cbca8282674
phuongthao101 <daongocphuongthao101@gmal.com>
2021-03-31 11:27:36 +0700

    upbai1

---
+++ module_2/src/bai_1_java_introduction/thuc_hanh/khai_bao_bien/Variable.java
@@ -0,0 +1,21 @@
+package bai_1_java_introduction.thuc_hanh.khai_bao_bien;
+
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Variable {
+    public static void main(String[] args) {
+        int i =10;
+        float f = 20.5f;
+        double d = 20.5d;
+        boolean b = true;
+        char c = 'a';
+        String s = "Hà Nội";
+        System.out.println("i = " +i);
+        System.out.println("f = " +f);
+        System.out.println("d = " +d);
+        System.out.println("b = " +b);
+        System.out.println("c = " +c);
+
+
+    }
+}
