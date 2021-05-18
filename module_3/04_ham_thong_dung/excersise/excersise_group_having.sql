5b4c9c219c07bdfa927d16b5d0d364a401ee8320
phuongthao101 <daongocphuongthao101@gmail.com>
2021-05-18 08:51:26 +0700

    update

--- module_3/04_ham_thong_dung/excersise/excersise_group_having.sql
+++ module_3/04_ham_thong_dung/excersise/excersise_group_having.sql
@@ -1,23 +1,23 @@
 use student_management;
 
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
-select *
+select *, max(sj.credit)
 from `subject` sj
 group by sj.sub_id
-having sj.credit >=all (select credit from `subject`);
+having max(sj.credit)  >=all (select max(sj.credit)  from `subject` sj group by sj.sub_id);
 
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-select *
+select `subject`.sub_id, `subject`.sub_name, mark.mark
 from `subject`
 inner join mark on mark.sub_id = `subject`.sub_id
-group by `subject`.sub_id
-having mark.mark >= all (select mark from mark);
+group by `subject`.sub_id, mark .mark
+having mark.mark >= all (select mark from mark group by mark.sub_id);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-select student.student_id, student.student_name, AVG(mark)
+select student.student_id, student.student_name, AVG(mark.mark )
 from student
-inner join mark on mark.student_id = student.student_id
+left join mark on mark.student_id = student.student_id
 group by student.student_id
 order by mark.mark DESC;
 -- done
