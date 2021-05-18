5d0318287a682428ebd4dde0714666869f51057d
phuongthao101 <daongocphuongthao101@gmail.com>
2021-05-18 15:17:55 +0700

    update

--- module_3/case_study_database/10_task_19_20.sql
+++ module_3/case_study_database/10_task_19_20.sql
@@ -9,3 +9,13 @@ from contract_detail
 where extra_service.extra_service_id = contract_detail.extra_service_id
 group by contract_detail.extra_service_id
 having count (contract_detail.extra_sevice_id ) >= 10 );
+-- task 20.	Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống,
+ -- thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
+select staff.staff_id, staff.`name`, staff.email,staff.phone,staff.birthday,staff.address		
+from staff
+union all
+select customer.customer_id,customer.customer_name, customer.email, customer.phone,customer.birthday,customer.address
+from customer;
+ -- done 
+
+
