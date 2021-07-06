afee4e902169c9a9cd2a26432e688960493ab19a
phuongthao101 <daongocphuongthao101@gmail.com>
2021-07-06 21:06:55 +0700

    update

--- module_4/CaseStudy_Spring_boot/src/main/java/com/codygym/model/entity/service/Service.java
+++ module_4/CaseStudy_Spring_boot/src/main/java/com/codygym/model/entity/service/Service.java
@@ -6,17 +6,17 @@ import javax.persistence.*;
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int service_id;
-    private String service_name;
-    private int service_area;
-    private double service_cost;
-    private int service_max_people;
-    private int rent_type_id;
-    private int service_type_id;
-    private String standard_room;
-    private String description_other_convenience;
-    private String pool_area;
-    private String number_of_floors;
+    private  Integer id;
+    private  String serviceName;
+    private  String serviceCode;
+    private int serviceArea;
+    private double serviceCost;
+    private int serviceMaxPeople;
+    private String standardRoom;
+    private String descriptionOtherConvenience;
+    private  double poolArea;
+    private int numberOfFloors;
+
     @ManyToOne
     @JoinColumn(name = "service_type_id", referencedColumnName = "serviceTypeId")
     private ServiceType serviceType;
@@ -25,12 +25,102 @@ public class Service {
     @JoinColumn(name = "rent_type_id", referencedColumnName = "rentTypeId")
     private RentType rentType;
 
-    public RentType getRentType() {
-        return rentType;
+    public Service() {
     }
 
-    public void setRentType(RentType rentType) {
+    public Service(Integer id, String serviceName, String serviceCode, int serviceArea, double serviceCost, int serviceMaxPeople, String standardRoom, String descriptionOtherConvenience, double poolArea, int numberOfFloors, ServiceType serviceType, RentType rentType) {
+        this.id = id;
+        this.serviceName = serviceName;
+        this.serviceCode = serviceCode;
+        this.serviceArea = serviceArea;
+        this.serviceCost = serviceCost;
+        this.serviceMaxPeople = serviceMaxPeople;
+        this.standardRoom = standardRoom;
+        this.descriptionOtherConvenience = descriptionOtherConvenience;
+        this.poolArea = poolArea;
+        this.numberOfFloors = numberOfFloors;
+        this.serviceType = serviceType;
         this.rentType = rentType;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getServiceCode() {
+        return serviceCode;
+    }
+
+    public void setServiceCode(String serviceCode) {
+        this.serviceCode = serviceCode;
+    }
+
+    public int getServiceArea() {
+        return serviceArea;
+    }
+
+    public void setServiceArea(int serviceArea) {
+        this.serviceArea = serviceArea;
+    }
+
+    public double getServiceCost() {
+        return serviceCost;
+    }
+
+    public void setServiceCost(double serviceCost) {
+        this.serviceCost = serviceCost;
+    }
+
+    public int getServiceMaxPeople() {
+        return serviceMaxPeople;
+    }
+
+    public void setServiceMaxPeople(int serviceMaxPeople) {
+        this.serviceMaxPeople = serviceMaxPeople;
+    }
+
+    public String getStandardRoom() {
+        return standardRoom;
+    }
+
+    public void setStandardRoom(String standardRoom) {
+        this.standardRoom = standardRoom;
+    }
+
+    public String getDescriptionOtherConvenience() {
+        return descriptionOtherConvenience;
+    }
+
+    public void setDescriptionOtherConvenience(String descriptionOtherConvenience) {
+        this.descriptionOtherConvenience = descriptionOtherConvenience;
+    }
+
+    public double getPoolArea() {
+        return poolArea;
+    }
+
+    public void setPoolArea(double poolArea) {
+        this.poolArea = poolArea;
+    }
+
+    public int getNumberOfFloors() {
+        return numberOfFloors;
+    }
+
+    public void setNumberOfFloors(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
     }
 
     public ServiceType getServiceType() {
@@ -41,125 +131,11 @@ public class Service {
         this.serviceType = serviceType;
     }
 
-    public Service(String name, String birthday, String id_card, Double employee_salary, String employee_phone, String employee_email, String employee_address, int position_id, int education_degree_id, int division_id, String username) {
+    public RentType getRentType() {
+        return rentType;
     }
 
-    public Service(int service_id, String service_name, int service_area, double service_cost, int service_max_people, int rent_type_id, int service_type_id, String standard_room, String description_other_convenience, String pool_area, String number_of_floors) {
-        this.service_id = service_id;
-        this.service_name = service_name;
-        this.service_area = service_area;
-        this.service_cost = service_cost;
-        this.service_max_people = service_max_people;
-        this.rent_type_id = rent_type_id;
-        this.service_type_id = service_type_id;
-        this.standard_room = standard_room;
-        this.description_other_convenience = description_other_convenience;
-        this.pool_area = pool_area;
-        this.number_of_floors = number_of_floors;
-    }
-
-    public Service(String service_name, int service_area, double service_cost, int service_max_people, int rent_type_id, int service_type_id, String standard_room, String description_other_convenience, String pool_area, String number_of_floors) {
-        this.service_name = service_name;
-        this.service_area = service_area;
-        this.service_cost = service_cost;
-        this.service_max_people = service_max_people;
-        this.rent_type_id = rent_type_id;
-        this.service_type_id = service_type_id;
-        this.standard_room = standard_room;
-        this.description_other_convenience = description_other_convenience;
-        this.pool_area = pool_area;
-        this.number_of_floors = number_of_floors;
-    }
-
-    public Service() {
-
-    }
-
-    public int getService_id() {
-        return service_id;
-    }
-
-    public void setService_id(int service_id) {
-        this.service_id = service_id;
-    }
-
-    public String getService_name() {
-        return service_name;
-    }
-
-    public void setService_name(String service_name) {
-        this.service_name = service_name;
-    }
-
-    public int getService_area() {
-        return service_area;
-    }
-
-    public void setService_area(int service_area) {
-        this.service_area = service_area;
-    }
-
-    public double getService_cost() {
-        return service_cost;
-    }
-
-    public void setService_cost(double service_cost) {
-        this.service_cost = service_cost;
-    }
-
-    public int getService_max_people() {
-        return service_max_people;
-    }
-
-    public void setService_max_people(int service_max_people) {
-        this.service_max_people = service_max_people;
-    }
-
-    public int getRent_type_id() {
-        return rent_type_id;
-    }
-
-    public void setRent_type_id(int rent_type_id) {
-        this.rent_type_id = rent_type_id;
-    }
-
-    public int getService_type_id() {
-        return service_type_id;
-    }
-
-    public void setService_type_id(int service_type_id) {
-        this.service_type_id = service_type_id;
-    }
-
-    public String getStandard_room() {
-        return standard_room;
-    }
-
-    public void setStandard_room(String standard_room) {
-        this.standard_room = standard_room;
-    }
-
-    public String getDescription_other_convenience() {
-        return description_other_convenience;
-    }
-
-    public void setDescription_other_convenience(String description_other_convenience) {
-        this.description_other_convenience = description_other_convenience;
-    }
-
-    public String getPool_area() {
-        return pool_area;
-    }
-
-    public void setPool_area(String pool_area) {
-        this.pool_area = pool_area;
-    }
-
-    public String getNumber_of_floors() {
-        return number_of_floors;
-    }
-
-    public void setNumber_of_floors(String number_of_floors) {
-        this.number_of_floors = number_of_floors;
+    public void setRentType(RentType rentType) {
+        this.rentType = rentType;
     }
 }
