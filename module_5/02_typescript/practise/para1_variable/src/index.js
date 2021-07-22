5cac525bbe1c980c551ef9977228043fc7dbd929
phuongthao101 <daongocphuongthao101@gmail.com>
2021-07-22 08:12:24 +0700

    update

---
+++ module_5/02_typescript/practise/para1_variable/src/index.js
@@ -0,0 +1,6 @@
+var wight;
+var height;
+wight = 10.5;
+height = 20;
+var area = wight * height;
+console.log("di\u1EC7n t\u00EDch: " + area);
---
+++ module_5/02_typescript/practise/para4_Rectangle/src/IIArea.ts
@@ -0,0 +1,6 @@
+interface IArea {
+    area(): number;
+}
+
+
+
